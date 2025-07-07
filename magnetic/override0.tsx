44f682178ebb331ec13091a1685c3d08fb509516
Daxesh Vadgama <daxhere@live.com>
2025-07-07 12:55:55 +1000

    added magnetic override

---
+++ magnetic/override0.tsx
@@ -0,0 +1,115 @@
+import { ComponentType, forwardRef } from "react"
+export function With05(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="05" />
+    ))
+}
+With05.displayName = "05% Magnetic Effect"
+export function With10(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="10" />
+    ))
+}
+With10.displayName = "10% Magnetic Effect"
+export function With15(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="15" />
+    ))
+}
+With15.displayName = "15% Magnetic Effect"
+export function With20(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="20" />
+    ))
+}
+With20.displayName = "20% Magnetic Effect"
+export function With25(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="25" />
+    ))
+}
+With25.displayName = "25% Magnetic Effect"
+export function With30(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="30" />
+    ))
+}
+With30.displayName = "30% Magnetic Effect"
+export function With35(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="35" />
+    ))
+}
+With35.displayName = "35% Magnetic Effect"
+export function With40(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="40" />
+    ))
+}
+With40.displayName = "40% Magnetic Effect"
+export function With45(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="45" />
+    ))
+}
+With45.displayName = "45% Magnetic Effect"
+export function With50(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="50" />
+    ))
+}
+With50.displayName = "50% Magnetic Effect"
+export function With55(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="55" />
+    ))
+}
+With55.displayName = "55% Magnetic Effect"
+export function With60(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="60" />
+    ))
+}
+With60.displayName = "60% Magnetic Effect"
+export function With65(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="65" />
+    ))
+}
+With65.displayName = "65% Magnetic Effect"
+export function With70(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="70" />
+    ))
+}
+With70.displayName = "70% Magnetic Effect"
+export function With75(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="75" />
+    ))
+}
+With75.displayName = "75% Magnetic Effect"
+export function With80(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="80" />
+    ))
+}
+With80.displayName = "80% Magnetic Effect"
+export function With85(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="85" />
+    ))
+}
+With85.displayName = "85% Magnetic Effect"
+export function With90(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="90" />
+    ))
+}
+With90.displayName = "90% Magnetic Effect"
+export function With95(Component): ComponentType {
+    return forwardRef((props, ref) => (
+        <Component ref={ref} {...props} data-magnet="95" />
+    ))
+}
+With95.displayName = "95% Magnetic Effect"
